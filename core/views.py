55a8c1af8fa4be3d0e5384fb77c77761812be2d3
Andrew <amstengel@quinnipiac.edu>
2015-11-12 20:00:47 +0000

    Add class create view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 
 class Home(TemplateView):
   template_name = "home.html"
+
+class ClassCreateView(CreateView):
+  model = Class
+  template_name = "class/class_form.html"
+  fields = ['course', 'professor']
+  success_url = reverse_lazy('home')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(ClassCreateView, self).form_valid(form)
